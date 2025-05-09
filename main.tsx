97b4fd4e1f083672cbfd1aa10391707559981fdd
root <root@vm3367596.firstbyte.club>
2025-05-09 18:53:47 +0000

    test commit

--- main.tsx
+++ main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from '../App' // Ensure this path is correct
 import './index.css'
 
+
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
 
